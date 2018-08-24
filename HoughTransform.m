0cec24de18812ceedf05966d7f78c9675eaca5de
Jiachen Sun <jiachen0203@gmail.com>
2018-08-24 09:36:35 -0400

    recognize left and right wall

--- HoughTransform.m
+++ HoughTransform.m
@@ -1,7 +1,7 @@
 if not(exist('msgs', 'Var'))
     msgs = loaddata('lidar-camera.bag');
 end
-for i=1:3:size(msgs)
+for i=1:10:size(msgs)
     msg = msgs{i}.copy;
 	msg.Ranges(msg.Ranges > 10) = NaN;
     houghAccumulator = zeros(180, 2000, 'uint8');
@@ -9,6 +9,7 @@ for i=1:3:size(msgs)
     x = msg.Ranges(180:900) .* cos(theta)'*100;
     y = msg.Ranges(180:900) .* sin(theta)'*100;
 
+    subplot(1,2,1);
     scatter(x, y, '.');
     grid on;
     axis equal;
@@ -16,7 +17,10 @@ for i=1:3:size(msgs)
 
     th = 0:1:179;
     d = round(x.*cos(pi.*th/180) + y.*(pi.*th/180))+1000;
-    maxSum = 0;
+    rightMax = 0;
+    leftMax = 0;
+    horizonMax = 0;
+    A = zeros(3, 3);
     for th=1:180
         for theta=1:721
             r = d(theta, th);
@@ -24,18 +28,33 @@ for i=1:3:size(msgs)
                 continue;
             end
             houghAccumulator(th, r) = houghAccumulator(th, r) + 1;
-            if (houghAccumulator(th,r) > maxSum)
-                maxSum = houghAccumulator(th,r);
-                A = [th, int32(r)-1000, maxSum];
+            if th < 60
+                if r > 1000 && houghAccumulator(th,r) > rightMax
+                    rightMax = houghAccumulator(th,r);
+                    A(1, :) = [th, int32(r)-1000, rightMax];
+                elseif r < 1000 && houghAccumulator(th,r) > leftMax
+                    leftMax = houghAccumulator(th, r);
+                    A(2, :) = [th, int32(r)-1000, leftMax];
+                end
             end
         end
     end
     xHough = linspace(-500,500,1001);
-    th = double(A(1))/180;
-    r = double(A(2));
-    yHough = (r-xHough*cos(th))/sin(th);
-    scatter(xHough, yHough, '.');
+    th = double(A(:,1))/180;
+    r = double(A(:,2));
+    yHough = (r-xHough.*cos(th))./sin(th);
+    scatter(xHough, yHough(1,:), '.');
+    scatter(xHough, yHough(2,:), '.');
     axis([-1000, 1000, 0, 2000]);
     hold off;
+    subplot(1,2,2);
+    mesh(houghAccumulator);
+    hold on
+    scatter(A(1,2)+1000, A(1,1), 'r');
+    scatter(A(2,2)+1000, A(2,1), 'g');
+    view([0,90]);
+    xlabel('r(cm)');
+    ylabel('th(deg)');
+    hold off
     drawnow;
 end
