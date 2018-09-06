bfbfbf210eb6e71cdd4c167d0e522bc3234fe666
Jiachen Sun <jiachen0203@gmail.com>
2018-09-06 16:10:09 -0400

    added comments to classifypoints

--- classifypoints.m
+++ classifypoints.m
@@ -1,17 +1,23 @@
 function [xOnLine,yOnLine,xNotOnLine,yNotOnLine] = classifypoints(x,y,r,th)
 %CLASSIFYPOINTS classify lidar points as wall or not wall
 %   Detailed explanation goes here
+
 distanceThresh = 0.25; % in meters
+
+% initialize params
 xOnLine = zeros(721, 1);
 yOnLine = zeros(721, 1);
 xNotOnLine = zeros(721, 1);
 yNotOnLine = zeros(721, 1);
 
-onCount = 0;
-notOnCount = 0;
+% calculate r estimation
 rHatRight = x(1:360)*cos(th(1))+y(1:360)*sin(th(1));
 rHatLeft = x(361:721)*cos(th(2))+y(361:721)*sin(th(2));
 rHat = [rHatRight; rHatLeft];
+
+% classify points
+onCount = 0;
+notOnCount = 0;
 for i=1:721
     if (abs(rHat(i) - r(floor(i/361) + 1)) < distanceThresh)
         onCount = onCount + 1;
@@ -24,6 +30,7 @@ for i=1:721
     end
 end
 
+% cut off zeros of classified points
 xOnLine = xOnLine(1:onCount);
 yOnLine = yOnLine(1:onCount);
 xNotOnLine = xNotOnLine(1:notOnCount);
